c2f75e8b08064d66a52a69038ffb50253c5072c6
Pouya Bisadi <pbisadi@klick.com>
2017-10-06 10:25:48 -0400

    First draft of random mulit weighted selection. WIP

--- DataStructures/Randomization/WeightedRandom.cs
+++ DataStructures/Randomization/WeightedRandom.cs
@@ -20,25 +20,55 @@ namespace Algorithm.Randomization
 		/// <param name="items"></param>
 		/// <param name="weight"></param>
 		/// <returns></returns>
-		public static IList<T> WeightedSelect<T>(this IList<T> items, int select_count, Func<T, double> weight)
+		public static IList<T> WeightedSelect<T>(this IList<T> items, int selectCount, Func<T, double> weight)
 		{
-			if (select_count < 1 || select_count > items.Count)
+			if (selectCount < 1 || selectCount > items.Count)
 				throw new ArgumentOutOfRangeException("The size of the selection must be a positive number equal or less than the size of the list");
 			items.Shuffle();
-			double sum = items.Select(i => weight(i)).Sum();
-			double chunk = sum / select_count;
+
 			var result = new List<T>();
+			var selected = new bool[items.Count];
+			SelectItems(items, weight, selectCount, ref selected);
 
 			for (int i = 0; i < items.Count(); i++)
 			{
+				if (selected[i]) result.Add(items[i]);
+			}
+			return result;
+		}
+
+		private static void SelectItems<T>(IList<T> items, Func<T, double> weight, int selectCount, ref bool[] selected)
+		{
+			if (selectCount == 0) return; //recursive base case
+
+			if (selected == null) selected = new bool[items.Count];
+			double sum = 0;
+			for (int i = 0; i < items.Count; i++)
+			{
+				sum += (!selected[i]) ? weight(items[i]) : 0;
+				if (selected[i]) selectCount--;
+			}
+			double chunk = sum / selectCount;
+			double rnd = _rnd.NextDouble() * chunk;
+
+			for (int i = 0; i < items.Count; i++)
+			{
 				if (rnd < weight(items[i]))
 				{
-					result.Add(items[i]);
+					selected[i] = true;
+					rnd += chunk;
 				}
+				else
 				rnd -= weight(items[i]);
+				if (rnd < 0) rnd += ((int)(-rnd / chunk) + 1) * chunk; // to skipp pieaces that are greater than chunk size
 			}
-			return items;
+			//TODO: It does not work this way :( if there are peices larger than chunk, we select less than desired count
+			// and we need to do the selection again. The challenge is that the selected items must be removed from the list
+			// to be able to converge to selecting desired count. If we remove items, then keeping that track of selected items
+			// by a flag array is not straight forward. So, I think a structure must be designed to combine selection flag and item.
 		}
+
+
 
 		public static int WeightedPick<T>(this IList<T> items, Func<T,double> weight)
 		{
