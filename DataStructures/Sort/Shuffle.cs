581b820f4f775a35e182da69570234830f71b735
Pouya Bisadi <p.bisadi@gmail.com>
2017-10-29 23:28:06 -0400

    Fix bug. The suffle algorithm & add more unit tests for random selection

--- DataStructures/Sort/Shuffle.cs
+++ DataStructures/Sort/Shuffle.cs
@@ -1,31 +1,39 @@
-﻿using System;
+﻿// from the following implementation
+//https://stackoverflow.com/questions/273313/randomize-a-listt
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Algorithm.Sort
 {
     public static class Shuffler
     {
-        public static void Shuffle<T>(this IList<T> list)
-        {
-            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
-            int n = list.Count;
-            while (n > 1)
-            {
-                byte[] box = new byte[1];
-                do 
-                    provider.GetBytes(box);
-                while (!(box[0] < n * (Byte.MaxValue / n)));
-                int k = (box[0] % n);
-                n--;
+        public static class ThreadSafeRandom
+		{
+			[ThreadStatic] private static Random Local;
 
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-    }
+			public static Random ThisThreadsRandom
+			{
+				get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+			}
+		}
+
+		public static void Shuffle<T>(this IList<T> list)
+		{
+			int n = list.Count;
+			while (n > 1)
+			{
+				n--;
+				int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
+		}
+	}
 }
--- UnitTestProject/RandomizationTests/WeightedRandomUnitTest.cs
+++ UnitTestProject/RandomizationTests/WeightedRandomUnitTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Algorithm.Randomization;
+using System.Linq;
 
 namespace UnitTestProject.RandomizationTests
 {
@@ -25,11 +26,25 @@ namespace UnitTestProject.RandomizationTests
 		}
 
 		[TestMethod]
-		[ExpectedException(typeof(ArgumentOutOfRangeException), "The size of the selection cannot be less than 1")]
 		public void WeightedSelect_SmokeTest()
 		{
 			var items = new List<int>() { 1, 2, 3, 4, 5 };
 			items.WeightedChoice(3, i => i);
 		}
+
+		[TestMethod]
+		public void WeightedSelect_SmokeTest2()
+		{
+			var items = new List<int>();
+			for (int i = 0; i < 1000000; i++) items.Add(1);
+			for (int i = 0; i < 1000000; i++) items.Add(2);
+			var result = items.WeightedChoice(10000, i => i);
+			int count1 = result.Count(item => item == 1);
+			int count2 = result.Count(item => item == 2);
+			//TODO: It was expected that the count1/count2 be very close to 0.5
+			// which is not the case in few runs (0.56). So, the make sure, implement
+			// round robin algorithm and compare the results.
+		}
+
 	}
 }
